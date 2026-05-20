a8eb733d4e32b71e19d7a4489921b9a22ccd2c3a
Thaciane de Paula Fernandes <info2020.20203029968@gmail.com>
2026-05-20 08:28:06 -0300

    Update Servico.java

--- treinamento/persiste/src/main/java/br/com/nsinova/biblioteca/persiste/postgres/Servico.java
+++ treinamento/persiste/src/main/java/br/com/nsinova/biblioteca/persiste/postgres/Servico.java
@@ -1,8 +1,10 @@
 package br.com.nsinova.biblioteca.persiste.postgres;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.nsinova.biblioteca.persiste.IServico;
@@ -19,20 +21,28 @@ import builders.ServicoBuilder;
     private StringBuilder montarSQL(){
         StringBuilder textoSQL = new StringBuilder();
         textoSQL.append("SELECT id");
-        textoSQL.append("from servico ");
+        textoSQL.append("from oficina.servico ");
         return textoSQL;
     }
 
      private br.com.nsinova.biblioteca.modelo.Servico montarItem(ResultSet resultSet) throws SQLException{
         Servico servico = ServicoBuilder.builder()
-                .setId(resultSet.getString("id"))
+                .setId(resultSet.getInt("id"))
                 .build();
     }
 
+    private List<br.com.nsinova.biblioteca.modelo.Servico> montarLista(ResultSet resultSet) throws SQLException{
+        List<br.com.nsinova.biblioteca.modelo.Servico> servicos = new ArrayList<>();
+        while (resultSet.next()) {            
+            servicos.add(montarItem(resultSet));
+        }
+        return servicos;
+    }
+
     @Override
-    public int manter(br.com.nsinova.biblioteca.modelo.Pessoa pessoa) throws SQLException {
+    public int manter(br.com.nsinova.biblioteca.modelo.Servico servico) throws SQLException {
         StringBuilder textoSQL = new StringBuilder();
-        textoSQL.append("INSERT INTO servico ");
+        textoSQL.append("INSERT INTO oficina.servico ");
         textoSQL.append("(id)");
         textoSQL.append("VALUES(?) ");
         
@@ -41,5 +51,19 @@ import builders.ServicoBuilder;
             return comando.executeUpdate();
         }
     }
+
+
+    @Override
+    public br.com.nsinova.biblioteca.modelo.Servico obter(int id) throws SQLException {
+        StringBuilder textoSQL = montarSQL();
+        textoSQL.append("WHERE id = ?");
+        try(PreparedStatement comando = conexao.prepareStatement(textoSQL.toString())){
+            comando.setInt(1, id);
+            try(ResultSet result = comando.executeQuery()){
+                return result.next() ? montarItem(result): null;
+            }
+        }
+    }
+
 
 }
