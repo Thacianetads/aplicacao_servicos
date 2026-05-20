54021c59e8dde1181f3a3201d9faf3c8e65c248a
Thaciane de Paula Fernandes <info2020.20203029968@gmail.com>
2026-05-20 08:27:45 -0300

    Update Pessoa.java

--- treinamento/persiste/src/main/java/br/com/nsinova/biblioteca/persiste/postgres/Pessoa.java
+++ treinamento/persiste/src/main/java/br/com/nsinova/biblioteca/persiste/postgres/Pessoa.java
@@ -1,12 +1,14 @@
 package br.com.nsinova.biblioteca.persiste.postgres;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.nsinova.biblioteca.persiste.IPessoa;
-import builders.PessoaBuilderBuilder;
+import builders.PessoaBuilder;
 import builders.ServicoBuilder;
 
 public class Pessoa implements IPessoa{
@@ -19,27 +21,47 @@ public class Pessoa implements IPessoa{
 
      private StringBuilder montarSQL(){
         StringBuilder textoSQL = new StringBuilder();
-        extoSQL.append("SELECT id");
-        textoSQL.append("from pessoa ");
+        textoSQL.append("SELECT id");
+        textoSQL.append("from oficina.pessoa ");
         return textoSQL;
     }
 
     private br.com.nsinova.biblioteca.modelo.Pessoa montarItem(ResultSet resultSet) throws SQLException{
         Pessoa pessoa = PessoaBuilder.builder()
-        .setId(resultSet.getString("id"))
+        .setId(resultSet.getInt("id"))
         .build();
+    }
+
+    private List<br.com.nsinova.biblioteca.modelo.Pessoa> montarLista(ResultSet resultSet) throws SQLException{
+        List<br.com.nsinova.biblioteca.modelo.Pessoa> pesssoas = new ArrayList<>();
+        while (resultSet.next()) {            
+            pessoas.add(montarItem(resultSet));
+        }
+        return pessoas;
     }
 
     @Override
     public int manter(br.com.nsinova.biblioteca.modelo.Pessoa pessoa) throws SQLException {
         StringBuilder textoSQL = new StringBuilder();
-        textoSQL.append("INSERT INTO pessoa ");
+        textoSQL.append("INSERT INTO oficina.pessoa ");
         textoSQL.append("(id)");
         textoSQL.append("VALUES(?) ");
         
         try(PreparedStatement comando = conexao.prepareStatement(textoSQL.toString())){
-            comando.setInt(1, servico.getPessoa()).getId();
+            comando.setInt(1, pessoa.getPessoa()).getId();
             return comando.executeUpdate();
+        }
+    }
+
+     @Override
+    public br.com.nsinova.biblioteca.modelo.Pessoa obter(int id) throws SQLException {
+        StringBuilder textoSQL = montarSQL();
+        textoSQL.append("WHERE id = ?");
+        try(PreparedStatement comando = conexao.prepareStatement(textoSQL.toString())){
+            comando.setInt(1, id);
+            try(ResultSet result = comando.executeQuery()){
+                return result.next() ? montarItem(result): null;
+            }
         }
     }
 }
