014c8fc55805b5ea4d23c1212ef552d353c26b83
Thaciane de Paula Fernandes <info2020.20203029968@gmail.com>
2026-05-20 08:28:27 -0300

    Update Veiculo.java

--- treinamento/persiste/src/main/java/br/com/nsinova/biblioteca/persiste/postgres/Veiculo.java
+++ treinamento/persiste/src/main/java/br/com/nsinova/biblioteca/persiste/postgres/Veiculo.java
@@ -1,10 +1,11 @@
 package br.com.nsinova.biblioteca.persiste.postgres;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-
 import br.com.nsinova.biblioteca.persiste.IVeiculo;
 import builders.ServicoBuilder;
 import builders.VeiculoBuilder;
@@ -18,7 +19,7 @@ public class Veiculo implements IVeiculo{
 
     private StringBuilder montarSQL(){
         StringBuilder textoSQL = new StringBuilder();
-        textoSQL.append("SELECT placa");
+        textoSQL.append("SELECT oficina.placa");
         textoSQL.append("from veiculo");
         return textoSQL;
     }
@@ -29,16 +30,36 @@ public class Veiculo implements IVeiculo{
         .build();
     }
 
+     private List<br.com.nsinova.biblioteca.modelo.Veiculo> montarLista(ResultSet resultSet) throws SQLException{
+        List<br.com.nsinova.biblioteca.modelo.Veiculo> veiculos = new ArrayList<>();
+        while (resultSet.next()) {            
+            veiculos.add(montarItem(resultSet));
+        }
+        return veiculos;
+    }
+
      @Override
-    public int manter(br.com.nsinova.biblioteca.modelo.Pessoa pessoa) throws SQLException {
+    public int manter(br.com.nsinova.biblioteca.modelo.Veiculo veiculo) throws SQLException {
         StringBuilder textoSQL = new StringBuilder();
-        textoSQL.append("INSERT INTO veiculo ");
+        textoSQL.append("INSERT INTO oficina.veiculo ");
         textoSQL.append("(placa)");
         textoSQL.append("VALUES(?) ");
         
         try(PreparedStatement comando = conexao.prepareStatement(textoSQL.toString())){
-            comando.setString(1, servico.getVeiculo()).getPlaca();
+            comando.setString(1, veiculo.getVeiculo()).getPlaca();
             return comando.executeUpdate();
+        }
+    }
+
+     @Override
+    public br.com.nsinova.biblioteca.modelo.Veiculo obter(String placa) throws SQLException {
+        StringBuilder textoSQL = montarSQL();
+        textoSQL.append("WHERE placa = ?");
+        try(PreparedStatement comando = conexao.prepareStatement(textoSQL.toString())){
+            comando.setString(1, placa);
+            try(ResultSet result = comando.executeQuery()){
+                return result.next() ? montarItem(result): null;
+            }
         }
     }
 }
